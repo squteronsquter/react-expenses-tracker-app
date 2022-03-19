571c42c0082e833bfe2a03dd31dd7c695a726acd
Digital Mind <jremesz@gmail.com>
2022-03-19 16:30:56 +0100

    ExpenseDate component created as independent component

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,21 +1,13 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "../ExpenseDate/ExpenseDate";
 
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const year = props.date.getFullYear();
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-
   return (
     <>
       <div className="expense-container">
-        <span className="expense-date">{props.id}</span>
+        <ExpenseDate date={props.date} />
         <h2 className="expense-title">{props.title}</h2>
         <p className="expense-amount">USD {props.amount}</p>
-        <div className="expense-date-wrapper">
-          <span className="expense-date-month">{month}</span>
-          <span className="expense-date-year">{year}</span>
-          <span className="expense-date-day">{day}</span>
-        </div>
       </div>
     </>
   );
