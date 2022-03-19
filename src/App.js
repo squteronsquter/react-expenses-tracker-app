078ca28cc57a4e057cada7b7db124f6c8792b4c8
Digital Mind <jremesz@gmail.com>
2022-03-19 12:59:50 +0100

    expenses day reformated to a calendar card

--- src/App.js
+++ src/App.js
@@ -31,19 +31,19 @@ function App() {
           id={expenses[0].id}
           title={expenses[0].title}
           amount={expenses[0].amount}
-          date={expenses[0].date.toISOString()}
+          date={expenses[0].date}
         />
         <ExpenseItem
           id={expenses[1].id}
           title={expenses[1].title}
           amount={expenses[1].amount}
-          date={expenses[1].date.toISOString()}
+          date={expenses[1].date}
         />
         <ExpenseItem
           id={expenses[2].id}
           title={expenses[2].title}
           amount={expenses[2].amount}
-          date={expenses[2].date.toISOString()}
+          date={expenses[2].date}
         />
       </div>
     </>
--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,13 +1,21 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const year = props.date.getFullYear();
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+
   return (
     <>
       <div className="expense-container">
         <span className="expense-date">{props.id}</span>
         <h2 className="expense-title">{props.title}</h2>
         <p className="expense-amount">USD {props.amount}</p>
-        <span className="expense-date">{props.date}</span>
+        <div className="expense-date-wrapper">
+          <span className="expense-date-month">{month}</span>
+          <span className="expense-date-year">{year}</span>
+          <span className="expense-date-day">{day}</span>
+        </div>
       </div>
     </>
   );
