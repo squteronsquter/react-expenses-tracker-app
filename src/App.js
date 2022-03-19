731d3939a53bbde607ab6c4db17ca4b5cf54a434
Digital Mind <jremesz@gmail.com>
2022-03-19 12:04:54 +0100

    date object created and passed as ISOSring to props

--- src/App.js
+++ src/App.js
@@ -6,20 +6,20 @@ function App() {
     {
       id: 123456,
       title: "Carand'Ache Crayons",
-      amount: 120,
-      date: "3/10/21",
+      amount: 120.49,
+      date: new Date(2022, 0, 10),
     },
     {
       id: 239908,
       title: "Arches Watercolor Paper",
-      amount: 220,
-      date: "8/10/21",
+      amount: 220.09,
+      date: new Date(2022, 1, 29),
     },
     {
       id: 882677,
       title: "Set of Winsor&Newton Brushes",
-      amount: 70,
-      date: "19/12/21",
+      amount: 70.99,
+      date: new Date(2022, 2, 19),
     },
   ];
 
@@ -31,19 +31,19 @@ function App() {
           id={expenses[0].id}
           title={expenses[0].title}
           amount={expenses[0].amount}
-          date={expenses[0].date}
+          date={expenses[0].date.toISOString()}
         />
         <ExpenseItem
           id={expenses[1].id}
           title={expenses[1].title}
           amount={expenses[1].amount}
-          date={expenses[1].date}
+          date={expenses[1].date.toISOString()}
         />
         <ExpenseItem
           id={expenses[2].id}
           title={expenses[2].title}
           amount={expenses[2].amount}
-          date={expenses[2].date}
+          date={expenses[2].date.toISOString()}
         />
       </div>
     </>
--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -6,7 +6,7 @@ function ExpenseItem(props) {
       <div className="expense-container">
         <span className="expense-date">{props.id}</span>
         <h2 className="expense-title">{props.title}</h2>
-        <p className="expense-amount">{props.amount}</p>
+        <p className="expense-amount">USD {props.amount}</p>
         <span className="expense-date">{props.date}</span>
       </div>
     </>
