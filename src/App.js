749ff862d2b39d616d07a5f160544d4fb3496023
Digital Mind <jremesz@gmail.com>
2022-03-19 16:57:38 +0100

    appjs made leaner by sourcing out expenses component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import ExpenseItem from "./components/ExpenseItem/ExpenseItem";
-function App() {
-  //Data source for my expenseitems
+import Expenses from "./components/Expenses/Expenses";
 
+function App() {
   const expenses = [
     {
       id: 123456,
@@ -27,24 +26,7 @@ function App() {
     <>
       <div className="container">
         <h1 className="header">My Expenses</h1>
-        <ExpenseItem
-          id={expenses[0].id}
-          title={expenses[0].title}
-          amount={expenses[0].amount}
-          date={expenses[0].date}
-        />
-        <ExpenseItem
-          id={expenses[1].id}
-          title={expenses[1].title}
-          amount={expenses[1].amount}
-          date={expenses[1].date}
-        />
-        <ExpenseItem
-          id={expenses[2].id}
-          title={expenses[2].title}
-          amount={expenses[2].amount}
-          date={expenses[2].date}
-        />
+        <Expenses items={expenses} />
       </div>
     </>
   );
---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,28 @@
+import ExpenseItem from "../ExpenseItem/ExpenseItem";
+
+function Expenses(props) {
+  return (
+    <>
+      <ExpenseItem
+        id={props.items[0].id}
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
+      />
+      <ExpenseItem
+        id={props.items[1].id}
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
+      />
+      <ExpenseItem
+        id={props.items[2].id}
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+        date={props.items[2].date}
+      />
+    </>
+  );
+}
+
+export default Expenses;
